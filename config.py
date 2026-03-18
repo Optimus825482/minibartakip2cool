79d75fa5e3ab370fa73e9d49a694f53a95078bc6
optmus825454 <ikinciyenikitap54@gmail.com>
2026-03-18 09:34:34 +0300

    update: config.py

--- config.py
+++ config.py
@@ -114,7 +114,7 @@ class Config:
         'X-Frame-Options': 'DENY',
         'X-XSS-Protection': '1; mode=block',
         'Referrer-Policy': 'strict-origin-when-cross-origin',
-        'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' cdn.jsdelivr.net cdn.datatables.net cdn.tailwindcss.com; style-src 'self' 'unsafe-inline' cdn.jsdelivr.net cdn.datatables.net fonts.googleapis.com; font-src 'self' fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self'",
+        'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' cdn.jsdelivr.net cdn.datatables.net cdn.tailwindcss.com code.jquery.com cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' cdn.jsdelivr.net cdn.datatables.net fonts.googleapis.com code.jquery.com cdnjs.cloudflare.com; font-src 'self' fonts.gstatic.com cdnjs.cloudflare.com; img-src 'self' data: https:; connect-src 'self' cdn.jsdelivr.net",
         'Strict-Transport-Security': 'max-age=31536000; includeSubDomains'  # HTTPS için
     }
     
